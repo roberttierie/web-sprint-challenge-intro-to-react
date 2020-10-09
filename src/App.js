50cdc9201791bb95c92ad45995ede397d738d05a
Robert Tierie <tangodolphin13@gmail.com>
2020-10-09 18:48:43 +0200

    DONE MVP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
+
 
 const App = () => {
+  const [charData, setCharData] = useState([])
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res=> {
+      console.log(res.data.results)
+      setCharData(res.data.results)
+    }).catch(err => {
+
+    })
+  }, [])
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,8 +25,11 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="App"> 
+      <h1 className="Header">Cast</h1>
+      {charData.map((char) => {
+        return <Character name={char.name} danger={char.name === 'Darth Vader'}/>
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
 // Write your Character component here
+
+import React from 'react';
+import styled, {keyframes} from 'styled-components'
+
+const kf = keyframes`
+100% {
+  opacity:1;
+
+}
+`
+
+
+const Character = (props) => {
+    const H2 = styled.h2`
+    color: orangered;
+    border: 2px solid black;
+    background-color:black;
+    margin-bottom: 10px;
+    border-radius: 25%;
+    display:flex;
+    justify-content:center;
+    flex: 1 1 20em;
+    margin-top: 25px;
+    margin-left: 600px;
+    margin-right: 600px;
+    animation: ${kf} 2.5s ease-in-out;
+    opacity:60%;
+    transition: all 0.25s ease-in-out;
+
+    &:hover {
+        text-decoration:underline;
+        color:yellow;
+        transform: scale(2);
+        opacity:100%;
+        
+    }
+    
+  `
+  
+    return (
+      <div className="App">
+        <H2>{props.name}</H2> 
+      </div>
+    );
+  }
+
+export default Character;
